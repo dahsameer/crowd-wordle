cef26bdedc3ee07dd3cbbacbc92ca55862a67ba0
Sameer Dahal <dahsameer@gmail.com>
2025-09-02 16:10:56 +0545

    fixed url

--- frontend/src/lib/shared/constants.ts
+++ frontend/src/lib/shared/constants.ts
@@ -1,11 +1,13 @@
-const HOST: string = "localhost:5240";
-function IS_TLS(): boolean {
-	return window.location.protocol == "https:";
+const isLocal = ["localhost", "127.0.0.1"].includes(window.location.hostname);
+const HOST = isLocal ? "localhost:5240" : window.location.host;
+
+function IS_TLS() {
+	return window.location.protocol === "https:";
 }
-export const HTTP_URL = IS_TLS() ? `https://${HOST}/api` : `http://${HOST}/api`;
-const WS_URL = IS_TLS() ? `wss://${HOST}/api/ws` : `ws://${HOST}/api/ws`;
+
+export const HTTP_URL = isLocal ? `http://${HOST}/api` : `/api`;
+const WS_URL = `${IS_TLS() ? "wss" : "ws"}://${HOST}/api/ws`;
 
 export function GET_WS_URL(token: string) {
-	console.log("token", token);
 	return `${WS_URL}?token=${token}`;
 }
